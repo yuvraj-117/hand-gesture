17fa3c4672a3b0ed71bd74caa8c258a531ead2d7
yuvraj-117 <71701495+yuvraj-117@users.noreply.github.com>
2021-05-10 14:21:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5I9bMnG-a/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+  
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The first prediction is " + prediction_1;
+  speak_data_2 = "And the second prediction is " + prediction_2;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterThis);
+}
+
+
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "yo")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#129304;";
+    }
+    if(results[0].label == "amazing")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#128076;";
+    }
+    if(results[0].label == "good")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#128077;";
+    }
+
+    if(results[0].label == "bad")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#128078;";
+    }
+    if(results[0].label == "victory")
+    {
+	    document.getElementById("update_gesture").innerHTML = "&#9996;";
+    }
+    if(results[1].label == "yo")
+    {
+	    document.getElementById("update_gesture2").innerHTML = "&#129304;";
+    }
+    if(results[1].label == "amazing")
+    {
+	    document.getElementById("update_gesture2").innerHTML = "&#128076;";
+    }
+    if(results[1].label == "good")
+    {
+	    document.getElementById("update_gesture2").innerHTML = "&#128077;";
+    }
+
+    if(results[1].label == "bad")
+    {
+	    document.getElementById("update_gesture2").innerHTML = "&#128078;";
+    }
+    if(results[1].label == "victory")
+    {
+	    document.getElementById("update_gesture2").innerHTML = "&#9996;";
+    }
+    
+  }
+}
+
